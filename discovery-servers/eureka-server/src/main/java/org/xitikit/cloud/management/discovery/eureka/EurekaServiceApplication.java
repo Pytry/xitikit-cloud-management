420e1c523608adc4ca0fe764d8b81f15933cd028
hoke1314 <keith.hoopes@brmutual.com>
2017-03-30 19:11:27 -0600

    Updated structure.

--- discovery-servers/eureka-server/src/main/java/org/xitikit/cloud/management/discovery/eureka/EurekaServiceApplication.java
+++ discovery-servers/eureka-server/src/main/java/org/xitikit/cloud/management/discovery/eureka/EurekaServiceApplication.java
@@ -1,4 +1,4 @@
-package org.xitikit.cloud.management.config;
+package org.xitikit.cloud.management.discovery.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ discovery-servers/eureka-server/src/test/java/org/xitikit/cloud/management/discovery/eureka/EurekaServiceApplicationTest.java
@@ -0,0 +1,20 @@
+package org.xitikit.cloud.management.discovery.eureka;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by hoke1314 on 3/30/2017.
+ *
+ */
+@SpringBootTest
+public class EurekaServiceApplicationTest {
+
+    @Test
+    public void load(){
+
+        assertTrue(true);
+    }
+}
