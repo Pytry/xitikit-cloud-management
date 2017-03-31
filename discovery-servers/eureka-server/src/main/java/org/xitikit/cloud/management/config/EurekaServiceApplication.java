e5230bb007be08c6ebfcff604779ab7d2edaf41d
hoke1314 <keith.hoopes@brmutual.com>
2017-03-30 19:05:07 -0600

    Initial commit

---
+++ discovery-servers/eureka-server/src/main/java/org/xitikit/cloud/management/config/EurekaServiceApplication.java
@@ -0,0 +1,14 @@
+package org.xitikit.cloud.management.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
+}
